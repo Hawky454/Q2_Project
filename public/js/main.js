ed5807fa8131e84a5a32b105a3e8b7db3cd0480d
David Miller <hawky454@gmail.com>
2018-07-10 09:18:07 -0500

    Completely re-styled my page and added cellar.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,1 @@
-console.log(name);
-
-function simpleFunc(name) {
-  name = prompt('Ello, stranger, please enter your name so that I can save it for later in my awesome database...!!!');
-  if(name === 'Dave') {
-    return alert(`Well, hello ${name}, you are no stranger afterall!  Where have you been my old friend?`);
-  } else {
-    console.log(name);
-    return alert(`It's a pleaseure to make your acquaintence, ${name}, I've added your beautiful name, ${name}, to our database. I hope to see you around here more often.`);
-  }
-}
-
-console.log(name);
-// simpleFunc();
+console.log('Hello stranger!');
