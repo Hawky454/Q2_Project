08feb514ecc3e56f7aa5f05b11fe6b0c2d0ab9e6
David Miller <hawky454@gmail.com>
2018-07-12 14:43:28 -0500

    Got all three tables in postgres to populate their respective pages now

---
+++ public/js/pipes.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetch('/pipes/')
+  .then(response => response.json())
+  .then(data => {
+     console.log(data);
+     data.forEach((pipesData) => {
+       console.log(pipesData);
+
+       let pipesCard = document.getElementById('pipes-card');
+
+       let pipesCardChild = document.createElement('section');
+       pipesCardChild.setAttribute('class','pipes-card-child');
+
+       let side1 = document.createElement('section');
+       side1.setAttribute('class', 'side1');
+
+       let side2 = document.createElement('section');
+       side2.setAttribute('class', 'side1');
+
+       let pipesImage = document.createElement('img');
+       pipesImage.setAttribute('src', pipesData.image);
+
+       let pipesBrand = document.createElement('h4');
+       pipesBrand.innerText = `Brand: ${pipesData.brand}`;
+
+       let pipeMaker = document.createElement('h4');
+       pipeMaker.innerText = `Pipemaker: ${pipesData.pipemaker}`;
+
+       let pipesYear = document.createElement('h4');
+       pipesYear.innerText = `Year: ${pipesData.year}`;
+
+       let pipesPrice = document.createElement('h4');
+       pipesPrice.innerText = `Price: ${pipesData.price.toFixed(2)}`;
+
+       let pipesEdit = document.createElement('button');
+       pipesEdit.setAttribute('class', 'edit-button');
+       pipesEdit.innerText = 'Edit Pipes';
+
+       let pipesDelete = document.createElement('button');
+       pipesDelete.setAttribute('class', 'delete-btn');
+       pipesDelete.innerText = 'Delete Pipes';
+
+       let sandblasted = document.createElement('h4');
+       sandblasted.innerText = `Sandblasted: ${pipesData.sandblasted}`;
+
+       let smooth = document.createElement('h4');
+       smooth.innerText = `Smooth: ${pipesData.smooth}`;
+
+       let pipesSource = document.createElement('h4');
+       pipesSource.innerText = `Source ${pipesData.source}`;
+
+       let pipesRating = document.createElement('h4');
+       pipesRating.innerText = `Rating: ${pipesData.rating} out of 5 stars`;
+
+
+       side1.appendChild(pipesImage);
+       side1.appendChild(pipesBrand);
+       side1.appendChild(pipeMaker);
+       side1.appendChild(pipesYear);
+       side1.appendChild(pipesPrice);
+
+       side2.appendChild(pipesEdit);
+       side2.appendChild(pipesDelete);
+       side2.appendChild(sandblasted);
+       side2.appendChild(smooth);
+       side2.appendChild(pipesSource);
+       side2.appendChild(pipesRating);
+
+       pipesCardChild.appendChild(side1);
+       pipesCardChild.appendChild(side2);
+
+       pipesCard.appendChild(pipesCardChild);
+
+     });
+  });
+});
