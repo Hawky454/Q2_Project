f41b3ac0573c81916f2d08fad931111743cc1199
David Miller <hawky454@gmail.com>
2018-07-13 08:57:47 -0500

    Second attempt at deploying app on heroku

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
 
     production: {
       client: 'pg',
-      connection: 'process.env.postgresql-metric-77959', //need an address here
+      connection: 'postgres://tsnqvlpyygrron:4cc8d09ad27798c88357d247a4f8101a706f637c12b138cc5d0b096e70e4f9d4@ec2-54-163-236-188.compute-1.amazonaws.com:5432/d18los8ol5uttc', //need an address here
     }
 
   };
